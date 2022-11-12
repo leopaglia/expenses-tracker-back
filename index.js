c757759dcbd3e1fdd0894033d58358b2a1408ad5
leopaglia <paglialungalf@gmail.com>
2022-11-11 22:00:51 -0300

    use node-cron for cron

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import cron from 'node-cron'
 
 import config from './config.js'
 import router from './src/router.js'
+
+import createExpensesFromRecurring from './src/jobs/createExpensesFromRecurring.js'
 
 const app = express()
 
@@ -14,4 +17,9 @@ app.use('/api/v1', router)
 
 app.listen(config.port, () => {
   console.log(`Server listening on http://localhost:${config.port}`)
+})
+
+cron.schedule('0 0 * * *', () => {
+  console.log('running scheduled job: createExpensesFromRecurring')
+  createExpensesFromRecurring()
 })
